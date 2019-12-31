fe47a130fdd54eb9cfd8a18946e5d69d5b2a58e8
jonkurtis <jonathankurtis@gmail.com>
2019-12-30 22:33:26 -0500

    Refactoring parse.js to run mjml before regex template replacing. Todo: Solve second promise returning undefined and preventing a file write to html email output.

--- parse.js
+++ parse.js
@@ -6,47 +6,98 @@ const fileIn = 'index'; // Put the name of your MJML template here (without the 
 const path = require('path')
 const mjml = path.join(__dirname, './src/index.mjml');
 
-const inject = (template, vars = {}) =>
+// const inject = (template, vars = {}) =>
+//   new Promise((resolve, reject) => {
+//     fs.readFile(
+//       template,
+//       { encoding: 'utf8' },
+//       (err, data) => {
+//         if (err) {
+//           return reject(err);
+//         }
+
+//         let finalTemplate = data;
+
+//         Object.keys(vars).forEach((key) => {
+//           const regex = new RegExp(`{{${key}}}`, 'g');
+//           finalTemplate = finalTemplate.replace(regex, vars[key]);
+//         });
+
+//         return resolve(finalTemplate);
+//       }
+//     );
+//   });
+
+// function compileTemplate(template) {
+ 
+//   let output = mjml2html(template, { minify: true });
+//   fs.writeFile(`./build/${fileIn}.html`, output.html, (err) => {
+//       if (err) {
+//           throw err;
+//       }
+//   })
+//   const hrend = process.hrtime(hrstart);
+//   console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
+
+// };
+
+// inject(mjml, {
+//   message: 'Goodbye Bob',
+// })
+// .then(finalTemplate => {
+//   // finalTemplate is an HTML string containing the template with all occurrences
+//   // of `{name}` replaced with "bob", and all occurrences of `{profileURL}`
+//   // replaced with "https://app.com/bob".
+//   compileTemplate(finalTemplate);
+// });
+
+const compileTemplate = (templatePath) =>
   new Promise((resolve, reject) => {
     fs.readFile(
-      template,
+      templatePath,
       { encoding: 'utf8' },
       (err, data) => {
         if (err) {
           return reject(err);
         }
 
-        let finalTemplate = data;
-
-        Object.keys(vars).forEach((key) => {
-          const regex = new RegExp(`{{${key}}}`, 'g');
-          finalTemplate = finalTemplate.replace(regex, vars[key]);
-        });
-
-        return resolve(finalTemplate);
+        let compiledTemplate = mjml2html(data, { minify: true });
+        let output = compiledTemplate.html;
+        return resolve(output);
       }
     );
   });
 
-function compileTemplate(template) {
- 
-  let output = mjml2html(template, { minify: true });
-  fs.writeFile(`./build/${fileIn}.html`, output.html, (err) => {
-      if (err) {
-          throw err;
-      }
-  })
-  const hrend = process.hrtime(hrstart);
-  console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
+function inject(template, vars = {}) {
+ new Promise((resolve, reject) => {
+   let finalTemplate = "Hello";
+    Object.keys(vars).forEach((key) => {
+      const regex = new RegExp(`{{${key}}}`, 'g');
+      finalTemplate = template.replace(regex, vars[key]);
+    });
+    return resolve(finalTemplate);
+  });
+ const hrend = process.hrtime(hrstart);
+ console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
 
 };
 
-inject(mjml, {
-  message: 'Goodbye Bob',
-})
-.then(finalTemplate => {
+function build(finalTemplate) {
+  console.log(finalTemplate);
+
+    fs.writeFile(`./build/${fileIn}.html`, finalTemplate, (err) => {
+      if (err) {
+          throw(err);
+      }
+  });
+}
+
+compileTemplate(mjml)
+.then((output) => {
   // finalTemplate is an HTML string containing the template with all occurrences
   // of `{name}` replaced with "bob", and all occurrences of `{profileURL}`
   // replaced with "https://app.com/bob".
-  compileTemplate(finalTemplate);
+  inject(output, {message: 'Hello Jane'});
+}).then((finalTemplate) => {
+   build(finalTemplate);
 });
